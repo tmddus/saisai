d7fdadaab2fd0552210cc0d227c4d95d5a590745
송승연 <wnzk413@gmail.com>
2019-01-18 10:23:17 +0900

    first commit

---
+++ app/src/main/java/sy/project2019/saisai/saisai/SplashActivity.java
@@ -0,0 +1,13 @@
+package sy.project2019.saisai.saisai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
