69f80a46c42c1bcc328f2b446f440cd4a50a0280
송승연 <wnzk413@gmail.com>
2019-01-18 10:34:50 +0900

    스플래시 구현

--- app/src/main/java/sy/project2019/saisai/saisai/SplashActivity.java
+++ app/src/main/java/sy/project2019/saisai/saisai/SplashActivity.java
@@ -1,5 +1,7 @@
 package sy.project2019.saisai.saisai;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(), 1500);
     }
+
+    private class splashhandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class)); //로딩이 끝난 후, ChoiceFunction 이동
+            SplashActivity.this.finish(); // 로딩페이지 Activity stack에서 제거
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
+
+
 }
